b0d62216f995e80ab7862b347e38d7e4f69ce113
Oumar Mimouni <omimouni@student.1337.ma>
2021-10-20 19:54:36 +0100

    ex00 megaphone

---
+++ c00/ex00/megaphone.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omimouni <omimouni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/19 19:01:19 by omimouni          #+#    #+#             */
+/*   Updated: 2021/10/20 19:45:56 by omimouni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+char	*ft_toupper(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		str[i] = std::toupper(str[i]);
+		i++;
+	}
+	return (str);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc > 1)
+	{
+		i = 1;
+		while (i < argc)
+		{
+			std::cout << ft_toupper(argv[i]);
+			i++;
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
+	return (0);
+}
