8c8170e74cb44d7be86542d2c6558d8eacd67e2b
Oumar Mimouni <omimouni@student.1337.ma>
2021-10-24 18:50:11 +0100

    Karening arround

--- c01/ex06/main.cpp
+++ c01/ex06/main.cpp
@@ -5,20 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: omimouni <omimouni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/23 20:30:33 by omimouni          #+#    #+#             */
-/*   Updated: 2021/10/24 17:29:17 by omimouni         ###   ########.fr       */
+/*   Created: 2021/10/24 18:42:33 by omimouni          #+#    #+#             */
+/*   Updated: 2021/10/24 18:49:34 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
 
-int	main()
+int	main(int argc, char **argv)
 {
-	Karen	karen;
+	int	ind;
 
-	karen.complain("DEBUG");
-	karen.complain("INFO");
-	karen.complain("WARNING");
-	karen.complain("ERROR");
+	if (argc == 2)
+	{
+		Karen	karen;
+		ind	= karen.level2index(argv[1]);
+		switch (ind)
+		{
+		case -1:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+			break;
+		case 0:
+			karen.complain("DEBUG");
+		case 1:
+			karen.complain("INFO");
+		case 2:
+			karen.complain("WARNING");
+		case 3:
+			karen.complain("ERROR");
+			break;
+
+		}
+	}
 	return (0);
 }
