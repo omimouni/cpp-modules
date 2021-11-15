64d8477fc5e37c5b3381de3a377687d12a44ddc4
Oumar Mimouni <omimouni@student.1337.ma>
2021-11-15 20:02:49 +0100

    sed is for losersss

--- c01/ex04/main.cpp
+++ c01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: omimouni <omimouni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 12:05:24 by omimouni          #+#    #+#             */
-/*   Updated: 2021/10/23 20:28:40 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/11/15 20:02:17 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,10 @@ int	main(int argc, char **argv)
 	if (argc > 3)
 	{
 		input.open(argv[1]);
-		output.open(std::string(argv[1]) + ".replace");		
-		std::string		origin(argv[2]);
+		output.open(std::string(argv[1]) + ".replace");
+		std::string		origin(argv[2]); // init string with char *
 		std::string		replace(argv[3]);
 
-		// BUG: it adds a line at the end
 		pos = -1;
 		while (std::getline(input, line))
 		{
