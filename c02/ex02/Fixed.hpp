a7ddd65b5e34d267e1989e15611df0648dfe2c7f
Oumar Mimouni <omimouni@student.1337.ma>
2021-11-17 18:59:39 +0100

    Fixed points

--- c02/ex02/Fixed.hpp
+++ c02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: omimouni <omimouni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 03:03:08 by omimouni          #+#    #+#             */
-/*   Updated: 2021/11/17 18:52:03 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/11/17 18:59:01 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,32 +19,39 @@ class Fixed {
     public:
 
         // Constructors
+
         Fixed();
         Fixed(const int number);
         Fixed(const float number);
         Fixed( const Fixed &f );
 
         // Descturctor
+
         ~Fixed();
         
         // raw bits manipulators
+
         int     getRawBits( void ) const;
         void    setRawBits( int const raw );
 
         // Floating and int representation
+
         float   toFloat( void ) const;
         int     toInt( void ) const;
 
         // Assignment operator
+
         Fixed & operator = (const Fixed &f);
 
         // Arithmetic operatos
+
         Fixed   operator + (Fixed f);
         Fixed   operator - (Fixed f);
         Fixed   operator * (Fixed f);
         Fixed   operator / (Fixed f);
 
         // Increment/Decrement operators
+
         Fixed   operator ++ (int x);
         Fixed   operator -- (int x);
         Fixed & operator ++ ();
@@ -60,6 +67,7 @@ class Fixed {
         bool    operator <= (const Fixed &other) const;
 
         // Static helper functions
+        
         static Fixed const& max(Fixed const &a, Fixed const &b);
         static Fixed & max(Fixed &a, Fixed &b);
         static Fixed & min(Fixed &a, Fixed &b);
